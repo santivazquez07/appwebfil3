40cfacd34639e772f7f2452de7fd1e9c5e55c51f
santivazquez07 <98130979+santivazquez07@users.noreply.github.com>
2022-09-02 21:37:18 -0500

    Add files via upload

---
+++ ma.js
@@ -0,0 +1,29 @@
+
+
+function save(){
+    
+}
+
+function preload(){
+
+}
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 600);
+    video.position(600, 120)
+    canvas = createCanvas(550, 450);
+    canvas.position(600 , 180)
+}
+
+function draw(){
+
+}
+
+function modelLoaded(){
+
+}
+
+function getPoses(results){
+
+}
